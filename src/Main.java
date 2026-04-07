327a0809ef4cfcbe4f61252ab14d987926edbbe8
ivan <vinogradovrozadorsyi@gmail.com>
2026-04-07 14:35:20 +0300

    home_work

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("________");
 
+        System.out.println("Задача 2");
+        System.out.println("Ф. И. О. сотрудника — " + fullName.toUpperCase());
+        System.out.println("________");
+
+        System.out.println("Задача 3");
+        firstName = "Семён";
+        middleName = "Семёнович";
+        lastName = "Иванов";
+        fullName = lastName + " " + firstName + " " + middleName;
+        String fullName2 = fullName.replace("ё","е");
+        System.out.println("Ф. И. О. сотрудника — " + fullName2);
+        System.out.println("________");
     }
 }
